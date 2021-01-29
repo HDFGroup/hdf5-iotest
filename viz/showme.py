31b8280b2cf13c3c681341ffcb727a9d2a5b78df
Gerd Heber <gheber@hdfgroup.org>
2021-01-29 17:02:05 -0600

    Show the reference triangle.

--- viz/showme.py
+++ viz/showme.py
@@ -1,23 +1,26 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.cm as cmx
+import sys
 
-df = pd.read_csv("hdf5_iotest.csv")
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
+fname = 'hdf5_iotest.csv'
+if (len(sys.argv) > 1):
+    fname = sys.argv[1]
 
-
-w = df.loc[:,'write-max [s]'].values
-r = df.loc[:,'read-max [s]'].values
+df = pd.read_csv(fname)
+wr = df.loc[:,'write-max [s]'].values
+rd = df.loc[:,'read-max [s]'].values
 cr = df.loc[:,'creat-max [s]'].values
 col = df.loc[:,'wall [s]']
 
-ax.scatter(w, r, cr, c=col, cmap=cmx.rainbow)
-for i in range(len(w)):
-    ax.text(w[i], r[i], cr[i], str(i), size=11, zorder=1, color='k')
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+ax.scatter(wr, rd, cr, c=col, cmap=cmx.rainbow)
+for i in range(len(wr)):
+    ax.text(wr[i], rd[i], cr[i], str(i), size=11, zorder=1, color='k')
 
 ax.set_xlabel('write (s)')
 ax.set_ylabel('read (s)')
-ax.set_zlabel('creat (s)')
+ax.set_zlabel('create (s)')
 
 plt.show()
